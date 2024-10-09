4eaac299924381634d3c53ddc108112ac01f0e06
mattadlerpdx <matt.adler.pdx@gmail.com>
2024-10-08 19:01:15 -0700

    updated api endpoint for bucket serving as static frontend

--- frontend/src/services/inventoryService.js
+++ frontend/src/services/inventoryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';  // This will be the base URL for your backend API
+const API_URL = 'https://cadence-backend-122826430682.us-west1.run.app';  // This will be the base URL for your backend API
 
 export const getInventory = async () => {
   try {
